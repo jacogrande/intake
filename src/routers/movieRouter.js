a4f554caa2a129bb47989451fce997e94d0c91a3
jacogrande <prowelljackson@gmail.com>
2020-03-23 22:06:04 -0700

    added src files

--- src/routers/movieRouter.js
+++ src/routers/movieRouter.js
@@ -57,8 +57,7 @@ movieRouter.route('/')
     const movieExists = await movieController.findMovieByTitle(apiData.Title);
 
     if (movieExists) { // if the movie exists in the db
-      const viewedByUser = () => (req.user.movies.indexOf(movieExists._id) === -1);
-      debug(viewedByUser);
+      const viewedByUser = (req.user.movies.indexOf(movieExists._id) === -1);
       if (!viewedByUser) { // if the movie exists but the user hasn't seen it
         movieController.addRating(ratings, movieExists._id); // add the user's ratings
         movieController.addThemes(themeData, movieExists._id);// and themes
